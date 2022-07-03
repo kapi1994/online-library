b035000bcea0d4395be3a174445910131ca40905
kapi1994 <nemanjajovicic13@gmail.com>
2022-07-04 01:01:11 +0200

    publisher page

--- includes/pages/admin/publisher.php
+++ includes/pages/admin/publisher.php
@@ -1,5 +1,23 @@
+<?php $publishers = getPublishers();
+$pages = publisherPagination();
+?>
 <main class="container">
     <div class="row mt-5">
+        <div class="col-lg-3 mb-2 mb-lg-0">
+            <input type="text" name="search_publisher" id="search_publisher" class="form-control" placeholder="Search">
+        </div>
+        <div class="col-lg-3">
+            <select name="order_publisher" id="order_publisher" class="form-select">
+                <option value="default">Sortirati po</option>
+                <option value="name_by_asc">Nazivu u rasutcem</option>
+                <option value="name_by_desc">Nazivu u opadajucem</option>
+                <option value="created_at_by_asc">Datumu kreiranja u rastucem</option>
+                <option value="created_at_by_desc">Datumu krairanja u opadajucem</option>
+            </select>
+        </div>
+    </div>
+    <div class="row my-2">
+        <div id="publisherResponseMessage"></div>
         <div class="col-lg-8">
             <div class="table-responsive-sm table-responsive-md">
                 <table class="table text-center align-middle">
@@ -9,21 +27,41 @@
                             <th scope="col">Name</th>
                             <th scope="col">Created at</th>
                             <th scope="col">Updated</th>
+                            <th scope='col'>Status</th>
                             <th scope="col">Edit</th>
                             <th scope="col">Change status</th>
+
                         </tr>
                     </thead>
-                    <tbody>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>Naziv</td>
-                            <td>11/11/1111</td>
-                            <td>11/11/1111</td>
-                            <td><button class="btn btn-sm btn-success" data-id='' type='button'>Edit</button></td>
-                            <td><button class="btn btn-sm btn-danger" type="button">Change status</button></td>
-                        </tr>
+                    <tbody id='publishers'>
+                        <?php
+                        $index = 1;
+                        foreach ($publishers as $publisher) : ?>
+                            <tr id='publisher_<?= $index ?>'>
+                                <th scope="row"><?= $index ?></th>
+                                <td><?= $publisher->name ?></td>
+                                <td><?= date('d/m/Y H:i:s', strtotime($publisher->created_at)); ?></td>
+                                <td><?= $publisher->updated_at != '' ? date('d/m/Y H:i:s', strtotime($publisher->updated_at)) : "-" ?></td>
+                                <td><?= $publisher->is_deleted == 0 ?  'Aktivan' : 'Neaktivan' ?></td>
+                                <td><button class="btn btn-sm btn-success btn-edit-publisher" data-id='<?= $publisher->id ?>' data-index='<?= $index ?>' type='button'>Edit</button></td>
+                                <td><button class="btn btn-sm btn-danger btn-change-status-publisher" data-id='<?= $publisher->id ?>' data-index='<?= $index ?>' data-status='<?= $publisher->is_deleted ?>'>
+                                        <?= $publisher->is_deleted == 0 ? 'Deaktiviraj' : 'Aktiviraj' ?>
+                                    </button></td>
+                            </tr>
+                        <?php $index++;
+                        endforeach; ?>
                     </tbody>
                 </table>
+            </div>
+            <div class="d-flex justify-content-center">
+                <nav aria-label="Page navigation example">
+                    <ul class="pagination" id='publisherPagination'>
+                        <?php
+                        for ($i = 0; $i < $pages; $i++) : ?>
+                            <li class="page-item <?php if ($i + 1 == 1) : ?> active <?php endif ?>"><a class="page-link publisher-pagination" data-id='<?= $i ?>' href="#"><?= $i + 1 ?></a></li>
+                        <?php endfor; ?>
+                    </ul>
+                </nav>
             </div>
         </div>
         <div class="col-lg-4">
