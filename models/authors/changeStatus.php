5e237fb7347d289300e793c99cf712729026a07f
kapi1994 <nemanjajovicic13@gmail.com>
2022-07-10 23:02:36 +0200

    soft delete for authors

---
+++ models/authors/changeStatus.php
@@ -0,0 +1,20 @@
+<?php
+
+header("Content-type:application/json");
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $id = $_POST['id'];
+    $status = $_POST['status'] != 0 ? 0 : 1;
+    // echo json_encode($status);
+    try {
+        require_once '../../config/connection.php';
+        include '../function.php';
+        changeStatusAuthor($id, $status);
+        $author  = getFullRow('author', 'id', $id);
+        echo json_encode($author);
+    } catch (PDOException $th) {
+        echo json_encode($th->getMessage());
+        http_response_code(500);
+    }
+} else {
+    http_response_code(404);
+}
