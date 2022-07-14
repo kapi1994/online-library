d3d086246341c66d35c124382bcf93f7f5f4623c
kapi1994 <nemanjajovicic13@gmail.com>
2022-07-14 23:39:09 +0200

    Added new pages

--- admin.php
+++ admin.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 require_once 'config/connection.php';
 require_once 'models/function.php';
 
@@ -8,16 +9,28 @@ $page = '';
 if (isset($_GET['page'])) {
     $page = $_GET['page'];
     switch ($page) {
-        case 'autori';
+        case 'authors';
             include 'includes/pages/admin/authors.php';
             break;
         case 'publishers':
             include 'includes/pages/admin/publisher.php';
             break;
-        case 'knjige':
-            include 'includes/pages/admin/book.php';
+        case 'books':
+            include 'includes/pages/admin/books.php';
+            break;
+        case 'book_editions':
+            include 'includes/pages/admin/books/book_editions.php';
+            break;
+        case 'users':
+            include 'includes/pages/admin/korisnici.php';
+            break;
+
+        case 'user-orders':
+            include 'includes/pages/admin/orders/orders.php';
             break;
     }
+} else {
+    include 'includes/pages/admin/index.php';
 }
 
 include 'includes/fixed/footer.php';
